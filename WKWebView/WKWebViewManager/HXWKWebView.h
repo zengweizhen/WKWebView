2d0312973b056715d35911783011dec30c566244
zengweizhen <18512166605@163.com>
2017-07-27 16:34:18 +0800

    WKWebView上传

---
+++ WKWebView/WKWebViewManager/HXWKWebView.h
@@ -0,0 +1,13 @@
+//
+//  HXWKWebView.h
+//  WKWebView
+//
+//  Created by Jney on 2017/7/27.
+//  Copyright © 2017年 Jney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXWKWebView : UIView
+
+@end
---
+++ WKWebView/WKWebViewManager/HXWKWebView.m
@@ -0,0 +1,21 @@
+//
+//  HXWKWebView.m
+//  WKWebView
+//
+//  Created by Jney on 2017/7/27.
+//  Copyright © 2017年 Jney. All rights reserved.
+//
+
+#import "HXWKWebView.h"
+
+@implementation HXWKWebView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
